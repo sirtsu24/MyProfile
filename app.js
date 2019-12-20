88a84e112b3ba989d1ec64d1961a7e254223873e
sirtsu24 <54986562+sirtsu24@users.noreply.github.com>
2019-12-20 19:15:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const ui = new UI;
 
 gitHub.getUserData()
 .then(data =>{
-        console.log(data.profile);
+        console.log(data.repos);
 
         ui.showProfile(data.profile);
         ui.showRepos(data.repos);
